9665c5f0b6af3609a70e0c2817b45a53e964a5f4
Admin <>
2026-02-05 08:56:22 +0200

    The FileListModel class was inherited from QAbstractListModel Base class' methods were overridden for future UI elements.

--- model/FileListModel.cpp
+++ model/FileListModel.cpp
@@ -1,3 +1,74 @@
 #include "FileListModel.h"
 
-FileListModel::FileListModel() {}
+#include <QFileInfo>
+#include <QHash>
+#include <QIcon>
+#include <QFileIconProvider>
+
+//NOTE: If Windows returns a file icon by its extension
+//NOTE: but not by its content (for example, a .png image)
+//NOTE: the hash can be optimized to store icons by file extension, rather than the path of each file.
+QHash<QString, QIcon> iconCache;
+
+FileListModel::FileListModel(AppModel& model, QObject* parent)
+    : QAbstractListModel(parent)
+    , model(model)
+{
+
+}
+
+
+int FileListModel::rowCount(const QModelIndex &parent) const
+{
+    return model.activeTab().files.size();
+}
+
+QVariant FileListModel::data(const QModelIndex &index, int role) const
+{
+    if(!index.isValid())
+        return {};
+
+    const auto& files = model.activeTab().files;
+
+    if(index.row() < 0 || index.row() >= files.size())
+        return {};
+
+    const FileItem& item = files[index.row()];
+
+    if(role == Qt::DisplayRole)
+    {
+        return QFileInfo(item.path).fileName();
+    }
+
+    if(role == Qt::ToolTipRole)
+    {
+        return item.path;
+    }
+
+    if(role == Qt::DecorationRole)
+    {
+        auto it = iconCache.find(item.path);
+        if(it != iconCache.end())
+            return it.value();
+
+        static QFileIconProvider iconProvider;
+        QIcon icon = iconProvider.icon(QFileInfo(item.path));
+        iconCache.insert(item.path, icon);
+        return icon;
+    }
+
+    return {};
+}
+
+void FileListModel::addFiles(const QStringList& paths)
+{
+    int before = model.activeTab().files.size();
+    model.addFilesToActiveTab(paths);
+
+    int after = model.activeTab().files.size();
+    if(after > before)
+    {
+        beginInsertRows(QModelIndex(), before, after - 1);
+        endInsertRows();
+    }
+}
--- model/FileListModel.h
+++ model/FileListModel.h
@@ -1,10 +1,23 @@
 #ifndef FILELISTMODEL_H
 #define FILELISTMODEL_H
 
-class FileListModel
+#include <QAbstractListModel>
+#include <QStringList>
+#include "../model/AppModel.h"
+
+
+class FileListModel : public QAbstractListModel
 {
+private:
+    Q_OBJECT
+    AppModel& model;
+
 public:
-    FileListModel();
+    FileListModel(AppModel& model, QObject* parent = nullptr);
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role) const override;
+    void addFiles(const QStringList& paths);
 };
 
 #endif // FILELISTMODEL_H
