08256fe3af08da3188729817314d16f124b328c0
Admin <>
2026-02-24 10:28:51 +0200

    Fix drawing icon

--- ui/FileItemDelegate.cpp
+++ ui/FileItemDelegate.cpp
@@ -15,28 +15,47 @@ void FileItemDelegate::paint(QPainter* painter,
     QStyleOptionViewItem opt = option;
     initStyleOption(&opt, index);
 
-    QStyle* style = opt.widget ? opt.widget->style() : QApplication::style();
+    QStyle* style = opt.widget
+        ? opt.widget->style()
+        : QApplication::style();
+    
     style->drawPrimitive(QStyle::PE_PanelItemViewItem, &opt, painter, opt.widget);
 
     painter->save();
+
+    QIcon icon     = index.data(Qt::DecorationRole).value<QIcon>();
+    QSize iconSize = opt.decorationSize;
+
+    QRect iconRect = QRect(
+    opt.rect.left() + 5,
+    opt.rect.top() + (opt.rect.height() - iconSize.height()) / 2,
+    iconSize.width(),
+    iconSize.height()
+    );
+
+    icon.paint(painter, iconRect);
+
+    int textLeft = iconRect.right() + 10;
 
     QString name  = index.data(Qt::DisplayRole).toString();
     QString path  = index.data(FileListModel::FilePathRole).toString();
     qint64  size  = index.data(FileListModel::FileSizeRole).toLongLong();
 
-    QRect r = option.rect;
+    QRect textRect = opt.rect.adjusted(textLeft, 5, -5, -5);
 
-    bool selected = option.state & QStyle::State_Selected;
-    auto pen = (selected ? option.palette.highlightedText().color() : option.palette.text().color());
+    bool selected = opt.state & QStyle::State_Selected;
+    
+    painter->setPen(selected
+            ? opt.palette.highlightedText().color()
+            : opt.palette.text().color());
 
-    painter->setPen(pen);
+    painter->drawText(textRect.adjusted(0, 0, 0, -20), name);
 
-    painter->drawText(r.adjusted(5, 5, -5, -25), name);
+    painter->setPen(selected 
+            ? opt.palette.highlightedText().color()
+            : QColor(120, 120, 120));
 
-    pen = (selected ? option.palette.highlightedText().color() : QColor(120, 120, 120));
-    painter->setPen(pen);
-
-    painter->drawText(r.adjusted(5, 25, -5, -5), path + " | " + QString::number(size) + " bytes");
+    painter->drawText(textRect.adjusted(0, 20, 0, 0), path + " | " + QString::number(size) + " bytes");
 
     painter->restore();
 }
