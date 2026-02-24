4e4f35286c36789c7d4bb39ead7f4550329c9264
Admin <>
2026-02-24 09:56:48 +0200

    Fix selection elements

--- ui/FileItemDelegate.cpp
+++ ui/FileItemDelegate.cpp
@@ -2,6 +2,7 @@
 #include "../model/FileListModel.h"
 
 #include <QPainter>
+#include <QApplication>
 
 FileItemDelegate::FileItemDelegate(QObject *parent)
     :QStyledItemDelegate(parent)
@@ -11,6 +12,12 @@ void FileItemDelegate::paint(QPainter* painter,
                              const QStyleOptionViewItem& option,
                              const QModelIndex& index) const
 {
+    QStyleOptionViewItem opt = option;
+    initStyleOption(&opt, index);
+
+    QStyle* style = opt.widget ? opt.widget->style() : QApplication::style();
+    style->drawPrimitive(QStyle::PE_PanelItemViewItem, &opt, painter, opt.widget);
+
     painter->save();
 
     QString name  = index.data(Qt::DisplayRole).toString();
@@ -18,7 +25,17 @@ void FileItemDelegate::paint(QPainter* painter,
     qint64  size  = index.data(FileListModel::FileSizeRole).toLongLong();
 
     QRect r = option.rect;
+
+    bool selected = option.state & QStyle::State_Selected;
+    auto pen = (selected ? option.palette.highlightedText().color() : option.palette.text().color());
+
+    painter->setPen(pen);
+
     painter->drawText(r.adjusted(5, 5, -5, -25), name);
+
+    pen = (selected ? option.palette.highlightedText().color() : QColor(120, 120, 120));
+    painter->setPen(pen);
+
     painter->drawText(r.adjusted(5, 25, -5, -5), path + " | " + QString::number(size) + " bytes");
 
     painter->restore();
